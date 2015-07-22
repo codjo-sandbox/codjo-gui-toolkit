c751fadb67fcf55500df9c3b4cd52f8b78b70d4f
Fabien DUMINY <fabien.duminy@allianzgi.fr>
2015-07-22 11:47:47 +0200

    Attempt to fix failing tests (test_frame_childAlwaysFront and test_frame_modalityCanBeRemoved) in ModalTest class.

--- src/test/java/net/codjo/gui/toolkit/AbstractJFCTestCase.java
+++ src/test/java/net/codjo/gui/toolkit/AbstractJFCTestCase.java
@@ -4,13 +4,18 @@ import java.awt.Component;
 import junit.extensions.jfcunit.JFCTestCase;
 
 public abstract class AbstractJFCTestCase extends JFCTestCase {
+    private static final int LOOP_DELAY = 10;
+    private static final int LOOP_TIMEOUT = 500;
+
+
     public void setFocus(Component component) {
         long start = System.currentTimeMillis();
         do {
             component.requestFocusInWindow();
             flushAWT();
+            sleep(LOOP_DELAY);
         }
-        while (!component.hasFocus() && System.currentTimeMillis() - start < 500);
+        while (!component.hasFocus() && System.currentTimeMillis() - start < LOOP_TIMEOUT);
         assertTrue("Les tests considèrent que le champ a le focus", component.hasFocus());
     }
 
@@ -20,8 +25,21 @@ public abstract class AbstractJFCTestCase extends JFCTestCase {
         do {
             component.transferFocus();
             flushAWT();
+            sleep(LOOP_DELAY);
         }
-        while (component.hasFocus() && System.currentTimeMillis() - start < 500);
+        while (component.hasFocus() && System.currentTimeMillis() - start < LOOP_TIMEOUT);
         assertTrue("Les tests considèrent que le champ n'a pas le focus", !component.hasFocus());
+    }
+
+
+    private static void sleep(int millis) {
+        if (millis > 0) {
+            try {
+                Thread.sleep(millis);
+            }
+            catch (InterruptedException e) {
+                ; // ignore
+            }
+        }
     }
 }
